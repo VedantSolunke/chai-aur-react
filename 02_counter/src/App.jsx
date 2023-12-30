d5513e5d296385226f3a8dae080cfff5acff249c
VedantSolunke <vedsocialid@gmail.com>
2023-12-30 12:21:01 +0530

    counter interview question added

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -6,7 +6,35 @@ function App() {
 
   const increaseVale = () => {
     if (counter <= 20) {
-      setCounter(counter + 1);
+      // setCounter(counter + 1);
+
+      setCounter((prevCounter) => {
+        prevCounter + 1;
+      });
+
+      /*
+        The code also includes comments explaining why multiple consecutive setCounter calls using the counter + 1
+        approach would not work as expected. React batches state updates to improve performance, and using the
+        functional update form ensures that each update is based on the latest state.
+        */
+
+      // Incorrect usage with multiple consecutive calls:
+      /*
+        setCounter(counter + 1);
+        setCounter(counter + 1);
+        setCounter(counter + 1);
+        setCounter(counter + 1);
+
+        Executing the above will NOT increase the value by 4 because React (useState) sends the data in batches.
+
+        Correct usage with functional update in multiple consecutive calls:
+        setCounter((prevCounter) => prevCounter + 1);
+        setCounter((prevCounter) => prevCounter + 1);
+        setCounter((prevCounter) => prevCounter + 1);
+        setCounter((prevCounter) => prevCounter + 1);
+
+        Executing the above WILL increase the value by 4.
+        */
     }
   };
 

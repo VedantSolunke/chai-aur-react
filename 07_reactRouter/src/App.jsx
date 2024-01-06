c3385c79dff07fd6e82960a98bbfbf9cc266cc77
VedantSolunke <vedsocialid@gmail.com>
2024-01-06 15:21:30 +0530

    07_react router added

---
+++ 07_reactRouter/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <h1 className=" p-4 bg-green-500">Reacter Router</h1> */}
+    </>
+  );
+}
+
+export default App;
